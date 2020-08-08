cd58ab932769606da9b0ae6f85016d47a777ec9b
DEVANSHTRIVEDI1606 <60578921+DEVANSHTRIVEDI1606@users.noreply.github.com>
2020-08-08 10:15:23 +0530

    1

---
+++ style.js
@@ -0,0 +1,97 @@
+function img1() {
+  var c = document.getElementById("can");
+  var ctx = c.getContext("2d");
+  var img = new Image();
+  img.src = "./collarimg/image_part_001.jpg"
+  
+  ctx.drawImage(img, 20, 20);
+}
+function img2() {
+  var c = document.getElementById("can");
+  var ctx = c.getContext("2d");
+  var img = new Image();
+  img.src = "./collarimg/image_part_002.jpg"
+  
+  ctx.drawImage(img, 20, 20);
+}
+function img3() {
+  var c = document.getElementById("can");
+  var ctx = c.getContext("2d");
+  var img = new Image();
+  img.src = "./collarimg/image_part_003.jpg"
+  
+  ctx.drawImage(img, 20, 20);
+}
+
+function img4() {
+  var c = document.getElementById("can");
+  var ctx = c.getContext("2d");
+  var img = new Image();
+  img.src = "./collarimg/image_part_004.jpg"
+  
+  ctx.drawImage(img, 20, 20);
+}
+function img5() {
+  var c = document.getElementById("can");
+  var ctx = c.getContext("2d");
+  var img = new Image();
+  img.src = "./collarimg/image_part_005.jpg"
+  
+  ctx.drawImage(img, 20, 20);
+}
+function img6() {
+  var c = document.getElementById("can");
+  var ctx = c.getContext("2d");
+  var img = new Image();
+  img.src = "./collarimg/image_part_006.jpg"
+  
+  ctx.drawImage(img, 20, 20);
+}
+function img7() {
+  var c = document.getElementById("can");
+  var ctx = c.getContext("2d");
+  var img = new Image();
+  img.src = "./collarimg/image_part_007.jpg"
+  
+  ctx.drawImage(img, 20, 20);
+}
+function img8() {
+  var c = document.getElementById("can");
+  var ctx = c.getContext("2d");
+  var img = new Image();
+  img.src = "./collarimg/image_part_008.jpg"
+  
+  ctx.drawImage(img, 20, 20);
+}
+function img9() {
+  var c = document.getElementById("can");
+  var ctx = c.getContext("2d");
+  var img = new Image();
+  img.src = "./collarimg/image_part_009.jpg"
+  
+  ctx.drawImage(img, 20, 20);
+}
+function img10() {
+  var c = document.getElementById("can");
+  var ctx = c.getContext("2d");
+  var img = new Image();
+  img.src = "./collarimg/image_part_010.jpg"
+  
+  ctx.drawImage(img, 20, 20);
+}
+function img11() {
+  var c = document.getElementById("can");
+  var ctx = c.getContext("2d");
+  var img = new Image();
+  img.src = "./collarimg/image_part_011.jpg"
+  
+  ctx.drawImage(img, 20, 20);
+}
+function img12() {
+  var c = document.getElementById("can");
+  var ctx = c.getContext("2d");
+  var img = new Image();
+  img.src = "./collarimg/image_part_012.jpg"
+  
+  ctx.drawImage(img, 20, 20);
+}
